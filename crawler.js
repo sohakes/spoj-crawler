4d5b5c10a6c8ac025898b1e4fc3c5bb2198f13eb
Sohakes <sohakes@gmail.com>
2016-05-01 23:30:34 -0300

    The site probably changed, and jsdom stopped working with it, fixed now

--- crawler.js
+++ crawler.js
@@ -10,22 +10,21 @@ link = "http://br.spoj.com"
 
 writeToFile = function(link, outResponse) {
   request({url: link, header: outResponse.headers}, function(error, response, html){
+    if(!error){
+      jsdom.env({html: html, done: function (err, window) {
+        txtarea = window.document.body.querySelector("#file").value
+        name = window.document.body.querySelector("[name=problemcode]").value
+        fs.writeFile("./submissions/"+name, txtarea, function(err) {
+          if(err) {
+            return console.log(err);
+          }
+        });
+        console.log("The file " + name + " was saved.");
 
-      if(!error){
-        jsdom.env({html: html, done: function (err, window) {
-          txtarea = window.document.body.querySelector("#file").value
-          name = window.document.body.querySelector("[name=problemcode]").value
-          fs.writeFile("./submissions/"+name, txtarea, function(err) {
-              if(err) {
-                  return console.log(err);
-              }
-          });
-          console.log("The file " + name + " was saved.");
-
-          // free memory associated with the window
-          window.close();
-        }});
-      }
+        // free memory associated with the window
+        window.close();
+      }});
+    }
   })
 }
 
@@ -33,19 +32,15 @@ getthing = function(outResponse, url){
   request({url: url, header: outResponse.headers}, function(error, response, html){
     if(!error){
       jsdom.env({html: html, done: function (err, window) {
-          links = window.document.body.querySelectorAll(".statusres")
-          for (i = 0; i < links.length; ++i) {
-            if(links[i].textContent.search("aceito") != -1) {
-              realLink = links[i].getElementsByTagName("a")[0]
-              relativePath = realLink.getAttribute("href")
-              if(relativePath != "#") {
-                editorPage = link + relativePath
-                console.log(editorPage)
-                writeToFile(editorPage, outResponse)
-              }
-            }
+        links = window.document.body.querySelectorAll(".kol1")
+        for (i = 0; i < links.length; ++i) {
+          if(links[i].textContent.search("accepted") != -1) {
+            code = links[i].querySelectorAll(".statustext")[0].textContent.trim()
+            problemName = /^.+\/(.+),/.exec(url)[1]
+            realLink = "http://br.spoj.com/submit/" + problemName + "/id=" + code
+            writeToFile(realLink, outResponse)
           }
-
+        }
         window.close();
       }});
     }
@@ -56,12 +51,15 @@ getPages = function(outResponse, url){
   request({url: url, header: outResponse.headers}, function(error, response, html){
     if(!error){
       jsdom.env({html: html, done: function (err, window) {
-          pages = window.document.body.querySelectorAll("#content table a")
-          for (i = 0; i < pages.length; ++i) {
+        pages = window.document.body.querySelectorAll("#content table a")
+        for (i = 0; i < pages.length; ++i) {
+          if (pages[i].innerHTML) {
             pageLink = pages[i].getAttribute("href")
+            console.log(pageLink + " " + pages[i].innerHTML)
             getthing(outResponse, link + pageLink)
           }
-          getthing(outResponse, url)
+        }
+        getthing(outResponse, url)
         window.close();
       }});
     }
@@ -71,11 +69,8 @@ getPages = function(outResponse, url){
 var login;
 var password;
 
-
-
 if(process.argv.length < 4) {
   var login = readlineSync.question('login: ');
-
   var password = readlineSync.question('password: ', {
     hideEchoBack: true
   });
@@ -85,5 +80,9 @@ if(process.argv.length < 4) {
 }
 
 request.post({url: "http://www.spoj.com/login/", form: {login_user: login, password: password}}, function(error, response, body){
-  getPages(response, "http://br.spoj.com/users/" + login + "/")
+  if (error) {
+    console.log("Error logging in");
+  } else {
+    getPages(response, "http://br.spoj.com/users/" + login + "/")
+  }
 })
